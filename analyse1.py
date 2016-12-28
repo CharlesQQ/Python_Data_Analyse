b73ff74d07216755fa02a73c31fd6bc3a212ad80
qiangqiang.chang <qiangqiang.chang@beebank.com>
2016-12-28 19:28:34 +0800

    引言第一部分

--- analyse1.py
+++ analyse1.py
@@ -38,9 +38,44 @@ frame = DataFrame(data=records)
 #print frame
 print frame['tz'][:10]
 
-clean_tz = frame['tz'].fillna('Missing')
-clean_tz[clean_tz == ''] ='Unknown'
+
+
+#下面利用matplotlib为这段数据生成一张图片，但是在这之前需要先给记录中未知或者缺失的时区填上一个替代值。
+clean_tz = frame['tz'].fillna('Missing')    #替换缺失值
+clean_tz[clean_tz == ''] ='Unknown'         #空字符串
 tz_counts = clean_tz.value_counts()
 print tz_counts[:10]
 
+tz_counts[:10].plot(kind='barh',rot=0)        #生成图片，一定要使用ipython的pylab模式打开
+print frame['a'][1]
+
+
+
+#将浏览器进行大致分类
+results = Series([x.split()[0] for x in frame.a.dropna()])
+print results[:5]
+
+
+#按照windows和非windows用户对时区统计信息进行分解
+cframe = frame[frame.a.notnull()]     #将agent缺失的数据移除
+operating_system = np.where(cframe['a'].str.contains('windows'),
+                            'Windows','Not Windows')
+
+print operating_system[:5]
+#根据时区和新得到的操作系统列表对数据进行分组
+by_tz_os = cframe.groupby(['tz',operating_system])
+agg_counts = by_tz_os.size().unstack().fillna(0)    #size对分组结果进行计数，利用unstack对计数结果进行重塑
+print agg_counts[:10]
+
+#选取最常出现的时区,agg_counts中的行数构造一个间接索引的数组
+indexer = agg_counts.sum(1).argsort()
+print indexer[:10]
+
+#使用take截取最后10行
+count_subset = agg_counts.take(indexer)[-10:]
+print count_subset
+
+count_subset.plot(kind='barh',stacked=True)    #生成一张堆积条形图
+normed_subset  = count_subset.div(count_subset.sum(1),axis=0)
+normed_subset.plot(kind='barh',stacked=True)    #将各行规范化为总计为1，并绘图
 

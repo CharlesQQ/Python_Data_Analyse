365d2c40d9ad7efb100e2426e5c2fa689f842627
qiangqiang.chang <qiangqiang.chang@beebank.com>
2017-03-22 09:18:56 +0800

    __slots__的使用

--- 算法/__slots__的使用.py
+++ 算法/__slots__的使用.py
@@ -6,12 +6,25 @@ class Student(object):
     __slots__=('name','age')
     gender='female'
     # name='china'
-    # pass
+    # age = 30
 
 s = Student()
+s1=Student()
 s.name = 'Charles'
 s.age=20
+print s.gender
 print s.name
+
+# print s1.name
 print s.age
 # s.gender='male'
+
+class PrimaryStudent(Student):
+    __slots__ = ('gender')
+s2 = PrimaryStudent()
+s2.name = 'eric'
+s2.gender='male'
+s2.grade=80
+print s2.name
+print s2.gender
 

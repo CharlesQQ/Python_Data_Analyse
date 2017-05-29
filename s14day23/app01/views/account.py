626c7aa01db75253f47a088c3c680fb13227fd86
charles <charles_ali@qq.com>
2017-05-29 10:46:50 +0800

    error序列化操作

--- s14day23/app01/views/account.py
+++ s14day23/app01/views/account.py
@@ -16,6 +16,16 @@ class LoginForm(forms.Form):
         max_length=64,
         min_length=12,
     )
+
+
+from django.core.exceptions import ValidationError
+class JsonCustomEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o,ValidationError):
+            return {'code':o.code,'messages':o.messages}
+        else:
+            return json.JSONEncoder.default(self,o)
+
 def login(request):
     if request.method == 'GET':
         return render(request,'login.html')
@@ -25,8 +35,29 @@ def login(request):
         if obj.is_valid():
             print(obj.cleaned_data)
         else:
-            print(type(obj.errors))   #from django.forms.utils import ErrorDict
+            print(type(obj.errors))   # <class'django.forms.utils.ErrorDict'>
+            #from django.forms.utils import ErrorDict
+
             print(obj.errors.as_json())
-            print(obj.errors.as_data())
-            ret['error'] = obj.errors.as_json()
-        return HttpResponse(json.dumps(ret))
+            """
+            Object {data: null, error: "{"password": [{"message": "Ensure this value has                            a…characters (it has 10).", "code": "min_length"}]}", status: true}
+            data
+            :
+            null
+            error
+            :
+            "{"password": [{"message": "Ensure this value has at least 12 characters (it has 10).",               "code": "min_length"}]}"
+            status
+            :
+            true
+            __proto__
+            :
+            Object
+            """
+            print(type(obj.errors.as_data()))
+            for k,v in obj.errors.as_data().items():
+                print(k,v)
+            #ret['error'] = obj.errors.as_json()   #在前端需要loads两次
+            ret['error'] = obj.errors.as_data()
+        result = json.dumps(ret,cls=JsonCustomEncoder)
+        return HttpResponse(json.dumps(result))

64cffa677299ca71ba0d38a912b6840fa11ccd48
charles <charles_ali@qq.com>
2017-06-04 18:59:56 +0800

    组合搜索条件，模板使用simple_tag，是的显示的内容更加简单;

--- s14day23/app01/models.py
+++ s14day23/app01/models.py
@@ -78,3 +78,22 @@ class UserInfo1(models.Model):
 
     def __str__(self):
         return self.username
+
+class Category(models.Model):
+    caption = models.CharField(max_length=16)
+
+
+class ArticleType(models.Model):
+    caption = models.CharField(max_length=16)
+
+class Article(models.Model):
+    title = models.CharField(max_length=32)
+    content = models.CharField(max_length=255)
+    category = models.ForeignKey(Category)
+    article_type = models.ForeignKey(ArticleType)
+    # type_chioce = (
+    #     (1,'python'),
+    #     (2,'openstack'),
+    #     (3,'Linux'),
+    # )
+    # article_type_id = models.IntegerField(choices=type_chioce)
